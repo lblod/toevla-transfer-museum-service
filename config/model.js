5967ee1c13864cb1ca7869af859fedb0f30c15d4
Aad Versteden <madnificent@gmail.com>
2021-05-06 00:17:22 +0200

    Extend the model to the full content of customizable data
    The model now contains the full customizable model.  That is everything
    that may be updated by a Museum.

--- config/model.js
+++ config/model.js
@@ -35,7 +35,7 @@ export default {
     "toevla:hasEscalator",
     "toevla:hasWheelchairAccessibleToilet",
     // relationships
-    "toevla:atLocation",
+    { uri: "toevla:atLocation", inverse: true },
     "toevla:hasFile",
     "toevla:hasImage",
     "toevla:hasEntrance",
@@ -86,6 +86,220 @@ export default {
     "toevla:smallestPointOnRouteForTarrace",
     "toevla:highestThresholdForTerrace",
     // no relationships
+  ],
+  "toevla:Shop": [
+    // properties
+    "toevla:accessControlWidth",
+    "toevla:mostNarrowDoorwidth",
+    "toevla:highestThreshold",
+    "toevla:smallestPointOnRoute",
+    "toevla:hasPaymentWithFixedElectronicPaymentSystem",
+    "toevla:hasPaymentWithMovableElectronicPaymentSystem",
+    "toevla:hasCashPayment",
+    "toevla:comment",
+    "toevla:heightOfLoweredCounter",
+    // no relationships
+  ],
+  "nfo:FileDataObject": [
+    // properties
+    "dbpedia:fileExtension",
+    "nfo:fileSize",
+    "dct:format",
+    "dct:description",
+    "rdfs:label",
+    "ext:order",
+    "nfo:fileName",
+    "nfo:fileCreated",
+    // relationships
+    { uri: "nie:dataSource", inverse: true },
+    // { uri: "toevla:hasFile", inverse: true } (exists in other direction)
+    // "toevla:belongsToExperienceTreeNodeScore" // not used yet
+  ],
+  "toevla:RouteDescription": [
+    // properties
+    "toevla:hasDetailedDescription",
+    "toevla:hasFlemishSignLanguage",
+    "toevla:hasScreenreader"
+  ],
+  "toevla:TrainStop": [
+    "toevla:distanceFromLocation",
+    "toevla:stopName"
+  ],
+  "toevla:TramStop": [
+    "toevla:distanceFromLocation",
+    "toevla:stopName"
+  ],
+  "toevla:BusStop": [
+    "toevla:distanceFromLocation",
+    "toevla:stopName"
+  ],
+  "https://linkedgeodata.org/ontology/Toilets": [
+    // properties
+    "toevla:amountOfSupportBraces",
+    "toevla:turningRadius",
+    "toevla:spaceNextToToilet",
+    "toevla:spaceInFrontOfToilet",
+    "toevla:turningRadiusAtDoor",
+    "toevla:doorWidth",
+    "toevla:typeOfElevator",
+    "toevla:amountOfPlateauElevators",
+    "toevla:hasRamps",
+    "toevla:amountOfSlopes",
+    "toevla:amountOfStairs",
+    "toevla:amountOfThresholds",
+    "toevla:highestThresholdOnRoute",
+    "toevla:smallestPointOnRoute",
+    "toevla:hasBabyNurturingTable",
+    "toevla:hasClearSignalizationInBuilding",
+    "toevla:hasSyntheticSpeechInElevator",
+    "toevla:hasSimpleAndLogicalRoute",
+    "toevla:isWheelchairAccessibleThroughBuilding",
+    "toevla:comment",
+    "toevla:hasWashbasin",
+    // relationships
+    "toevla:sizeOfPlateauElevator",
+    "toevla:sizeOfToiletRoom",
+    "toevla:canRideUnderWashbasinCategory",
+    "toevla:sizeOfElevator"
+  ],
+  "toevla:Area": [
+    // properties
+    "toevla:widthInCm",
+    "toevla:heightInCm",
+  ],
+  "schema:Parking": [
+    // properties
+    "toevla:parkingSpaceWidth",
+    "toevla:parkingSpaceLength",
+    "toevla:hasWheelchairFriendlyTerrain",
+    "toevla:onPublicDomain",
+    "toevla:numberOfWheelchairFriendlySpots",
+    "toevla:detailedRouteDescriptionIsAvailableInFlemishSignLanguage",
+    "toevla:detailedRouteDescriptionHasScreenReader",
+    "toevla:hasDetailedRouteDescription",
+    "toevla:isWellLit",
+    "toevla:hasDriveOnPossibility",
+    "toevla:isPartOfLocation",
+    "toevla:comment",
+    "toevla:maxVehicleHeight",
+    // relationships
+    "toevla:pathToEntrance"
+  ],
+  "toevla:Path": [
+    // properties
+    "toevla:amountOfSlopes",
+    "toevla:amountOfStairs",
+    "toevla:numberOfThresholds",
+    "toevla:highestThreshold",
+    "toevla:narrowestPoint",
+    "toevla:hasWheelchairFriendlyTerrain",
+    "toevla:hasRamp",
+    // no relationships
+  ],
+  "toevla:Entrance": [
+    // properties
+    "toevla:turningRadiusAtDoor",
+    "toevla:hasEntranceCheck",
+    "toevla:doorWidth",
+    "toevla:hasRevolvingDoor",
+    "toevla:hasRamps",
+    "toevla:amountOfSlopes",
+    "toevla:amountOfStairs",
+    "toevla:amountOfThresholds",
+    "toevla:commentOnAlternativeEntranceForWheelchair",
+    "toevla:highestThreshold",
+    "toevla:hasTeleloopAtCounter",
+    "toevla:hasMannedDesk",
+    "toevla:hasVisibleGuidelines",
+    "toevla:hasLoweredCounter",
+    // relationships
+    // { uri: "toevla:hasEntrance", inverse: true } // on Poi
+    "toevla:forceForOpeningDoorCategory"
+  ],
+  "toevla:Experience": [
+    // properties
+    "toevla:hasPlacesOfSilence",
+    "toevla:hasListeningElements",
+    "toevla:hasActionableElements",
+    "toevla:hasSmellElements",
+    "toevla:hasTouchElements",
+    "toevla:hasVirtualTechnology",
+    "toevla:hasMovieGuide",
+    "toevla:audioGuideAvailableViaOtherMeans",
+    "toevla:audioGuideAvailableViaSmartphone",
+    "toevla:audioGuideAvailableViaHeatset",
+    "toevla:audioGuideAvailableInMultipleLanguages",
+    "toevla:audioGuideOnlyInDutch",
+    "toevla:signsInLargePrint",
+    "toevla:signsHaveBraille",
+    "toevla:signsHaveNoReflection",
+    "toevla:signsHaveAccessibleContrast",
+    "toevla:brochureIsAvailableInLargePrint",
+    "toevla:brochureIsAvailableInBraille",
+    "toevla:brochureHasAccessibleContrast",
+    "toevla:hasMagnifyingGlass",
+    "toevla:hasGoodLighting",
+    "toevla:hasMultipleElementsToDriveUnder",
+    "toevla:comment",
+    "dct:title",
+    "toevla:otherElements",
+    // relationships
+    "toevla:scoreSubject",
+    "toevla:hasAuditorium",
+    "toevla:guidedTour",
+    "toevla:hasCirculation",
+    "toevla:atLocation",
+    { uri: "toevla:scoreSubject", inverse: true }
+  ],
+  "toevla:Auditorium": [
+    // noproperties
+    // relationships
+    "toevla:availabilityOfTailorMadeTours",
+    "toevla:availabilityOnQuietMoments",
+    "toevla:supportForChildren",
+    "toevla:supportForAutism",
+    "toevla:supportForDementia",
+    "toevla:supportForMentalHandicap",
+    "toevla:supportForDeaf",
+    "toevla:supportForAuditiveHandicap",
+    "toevla:supportForVisualHandicap",
+  ],
+  "toevla:Route": [
+    // properties
+    "toevla:hasMultipleRestingAndSittingOpportunities",
+    "toevla:hasAlternativePathForHardToAccessSpaces",
+    "toevla:hasPlanForAdvisedPath",
+    "toevla:hasVenuePlan",
+    "toevla:hasClearSignalizationOnRoute",
+    "toevla:typeOfElevator",
+    "toevla:amountOfPlateauElevators",
+    "toevla:hasRamps",
+    "toevla:amountOfSlopes",
+    "toevla:amountOfStairs",
+    "toevla:amountOfThresholds",
+    "toevla:highestThresholdOnRoute",
+    "toevla:smallestPointOnRoute",
+    "toevla:hasSyntheticSpeechInElevator",
+    "toevla:hasClearSignalizationInBuilding",
+    "toevla:isSimpleAndLogical",
+    "toevla:comment",
+    "toevla:hasExtraAttentionForAcoustics",
+    // relationships
+    "toevla:multipleLevelsCategory",
+    "toevla:sizeOfPlateauElevator",
+    "toevla:sizeOfElevator",
+  ],
+  "toevla:TreeNodeScore": [
+    // properties
+    "toevla:commentLinkUrl",
+    "toevla:commentLinkText",
+    "rdfs:comment",
+    "toevla:score",
+    "ext:entryWasVisited",
+    // relationships
+    "toevla:selectedConcept",
+    "toevla:scoreTopic",
+    "toevla:scoreSubject"
   ]
 };
 
